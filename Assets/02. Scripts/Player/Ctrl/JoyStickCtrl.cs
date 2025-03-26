6fad04c66cf90912638ac523a8aa5e2a95716fa4
wnsdud3095 <wnsdud30954@naver.com>
2025-03-27 01:09:37 +0900

    feat : 플레이어 터치 조이스틱 구현

---
+++ Assets/02. Scripts/Player/Ctrl/JoyStickCtrl.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStickCtrl : MonoBehaviour, IDragHandler , IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private RectTransform m_back_ground;
+    [SerializeField]
+    private RectTransform m_handle;
+
+    private float m_back_ground_radius;
+
+    private Vector2 m_start_position;
+    private Vector2 m_input_vector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_back_ground_radius = m_back_ground.rect.width / 2;
+        m_back_ground.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("ÅÍÄ¡µÊ");
+        m_start_position= eventData.position;
+        m_back_ground.position= m_start_position;
+        m_back_ground.gameObject.SetActive(true);
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("µå·¡±×µÊ");
+        Vector2 dir = eventData.position - m_start_position;
+        m_input_vector = Vector2.ClampMagnitude(dir, m_back_ground_radius);
+        m_handle.anchoredPosition= m_input_vector;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("ÅÍÄ¡³¡");
+        m_input_vector = Vector2.zero;
+        m_handle.anchoredPosition = m_input_vector;
+        m_back_ground.gameObject.SetActive(false) ; 
+    }
+
+    public Vector2 GetInputVector()
+    {
+        return m_input_vector != null ? m_input_vector/ m_back_ground_radius : Vector2.zero;
+    }
+
+}
---
+++ Assets/02. Scripts/Player/Ctrl/PlayerCtrl.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private JoyStickCtrl m_joy_stick;
+    private Rigidbody2D m_rigid;
+    private SpriteRenderer m_sprite_renderer;
+
+    private float m_move_speed = 3f;
+
+    void Start()
+    {
+        m_joy_stick = GameObject.Find("TouchPanel").GetComponent<JoyStickCtrl>();
+        m_rigid = GetComponent<Rigidbody2D>();
+        m_sprite_renderer= GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 input_vector = m_joy_stick.GetInputVector();
+        m_rigid.linearVelocity = new Vector2(input_vector.x * m_move_speed, input_vector.y * m_move_speed);
+        if(input_vector.x> 0 )
+        {
+            m_sprite_renderer.flipX= false;
+        }
+        else
+        {
+            m_sprite_renderer.flipX = true;
+
+        }
+    }
+}
