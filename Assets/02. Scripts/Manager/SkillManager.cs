7c7a74ee1e1dbdb7be96575ccf1b35278cc354e7
wnsdud3095 <wnsdud30954@naver.com>
2025-04-23 23:07:09 +0900

    fix : test 코드 제거

--- Assets/02. Scripts/Manager/SkillManager.cs
+++ Assets/02. Scripts/Manager/SkillManager.cs
@@ -35,7 +35,6 @@ public class SkillManager : MonoBehaviour
     void Start()
     {
         GetWeaponSkill(GameManager.Instance.CalculatedStat.WeaponID);
-        AddSkill(21);
     }
 
     public void GetWeaponSkill(int id)
